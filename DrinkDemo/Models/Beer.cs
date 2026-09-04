73686d414579da1ee64b785429e312c977940fed
Matthew Eberhart <mattheweberhart75@gmail.com>
2026-09-03 21:25:11 -0700

    Added file Beer.cs to Models folder. Added first implementation of the Beer class.

---
+++ DrinkDemo/Models/Beer.cs
@@ -0,0 +1,23 @@
+﻿//*********************************************************
+//
+// Beer.cs
+//
+// Defines the Beer class.
+// Inherits from the Drink class.
+// Defines unique behavior for keeping the alcohol content percentage of the beer.
+//
+
+namespace DrinkDemo.Models
+{
+    public class Beer : Drink
+    {
+        public double alcohol_content_percentage { get; }
+
+        public Beer(string drink_name, bool is_carbonated, double alcohol_content_percentage) : base(drink_name, is_carbonated)
+        {
+            this.alcohol_content_percentage = alcohol_content_percentage;
+        }
+
+        public override string description => $"{base.description}, {alcohol_content_percentage}%";
+    }
+}
