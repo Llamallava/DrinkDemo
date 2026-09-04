68eb85cab89c8a2a9bd3160aa6802a0da8dfea2a
Matthew Eberhart <mattheweberhart75@gmail.com>
2026-09-03 21:07:59 -0700

    Created new folder Models. Created new file Drink.cs Edited Drink.cs with first implementation of abstract Drink class.

---
+++ DrinkDemo/Models/Drink.cs
@@ -0,0 +1,32 @@
+﻿//*********************************************************
+//
+// Drink.cs
+//
+//
+//
+
+namespace DrinkDemo.Models
+{
+    public abstract class Drink
+    {
+        public string drink_name { get; }
+        public bool is_carbonated { get; }
+
+        public Drink(string drink_name, bool is_carbonated)
+        {
+            this.drink_name = drink_name;
+            this.is_carbonated = is_carbonated;
+        }
+
+        public virtual string description
+        {
+            get
+            {
+                string carbonation_text = is_carbonated ? "carbonated" : "not carbonated";
+                return $"{drink_name}, {carbonation_text}";
+            }
+        }
+
+        public override string ToString() => description;
+    }
+}
